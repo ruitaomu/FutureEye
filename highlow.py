60975711b8122f2aafeba8fb5709824eb0816b1d
Max Mu <ruitao.mu@gmail.com>
2023-09-19 02:13:58 +0800

    Support manual marked data
    1. add AUTO_MARK flags
    2. use double-stack RNN instead of single-layer RNN
    3. use K-line figure for training result display

--- highlow.py
+++ highlow.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler 
 from sklearn.metrics import mean_squared_error
@@ -15,15 +16,24 @@ N_STEPS = 8
 FEATURES = 2
 FEATURE_OFFSET = 1
 IGNORE_SAMPLE_FINISH_WITH_MINMAX = False
+AUTO_MARK = True
 
 train_set_X_list = list()
 train_set_y_list = list()
 test_set_X_list = list()
 test_set_y_list = list()
 
+n_top_samples = 0
+n_bottom_samples = 0
+
 def add_train_set(sample_seq, value):
+    global n_top_samples, n_bottom_samples
     n_seq = len(sample_seq)
     if (n_seq > 0):
+        if value > 0:
+            n_top_samples += n_seq
+        else:
+            n_bottom_samples += n_seq
         train_set_X_list.extend(sample_seq)
         res = np.full((n_seq,), value)
         train_set_y_list.extend(res.tolist())
@@ -38,36 +48,46 @@ def add_test_set(sample_seq, value):
 def process_data_file(data_file_name):
     high_indeces = []
     low_indeces = []
-    highest_value = 0.0
-    lowest_value = 0.0
     dataset = pd.DataFrame()
 
     set_seed(4550)
     np.random.seed(4551)
 
     dataset = pd.read_csv(
-        data_file_name
+        data_file_name, dtype={"Top": int, "Bottom": int, "High": float, "Low": float, "Open": float, "Close": float}
     )
 
     #print(dataset.head())
     #print(dataset.describe())
 
-    #找出给定图中所有的高低点位
-    #all_high_points = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"High"].to_numpy()
-    #all_low_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Low"].to_numpy()
-    all_close_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Close"].to_numpy()
+    global AUTO_MARK
+    if AUTO_MARK == False and 'Top' in dataset.columns and 'Bottom' in dataset.columns:
+        AUTO_MARK = False
+    else:
+        AUTO_MARK = True
 
-    #找出全图最高和最低点的值
-    #highest_value = np.max(all_high_points)
-    #lowest_value  = np.min(all_low_points)
-    highest_value = np.max(all_close_points)
-    lowest_value  = np.min(all_close_points)
+    if AUTO_MARK == True:        
+        #找出给定图中所有的高低点位
+        #all_high_points = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"High"].to_numpy()
+        #all_low_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Low"].to_numpy()
+        all_close_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Close"].to_numpy()
 
-    #找出最高和最低点所在的位置，保存在数组中。有可能不止一个最高或最低点
-    #high_indeces = np.where(all_high_points == highest_value)[0]+N_STEPS
-    #low_indeces = np.where(all_low_points == lowest_value)[0]+N_STEPS
-    high_indeces = np.where(all_close_points == highest_value)[0]+N_STEPS
-    low_indeces = np.where(all_close_points == lowest_value)[0]+N_STEPS
+        #找出全图最高和最低点的值
+        #highest_value = np.max(all_high_points)
+        #lowest_value  = np.min(all_low_points)
+        highest_value = np.max(all_close_points)
+        lowest_value  = np.min(all_close_points)
+
+        #找出最高和最低点所在的位置，保存在数组中。有可能不止一个最高或最低点
+        #high_indeces = np.where(all_high_points == highest_value)[0]+N_STEPS
+        #low_indeces = np.where(all_low_points == lowest_value)[0]+N_STEPS
+        high_indeces = np.where(all_close_points == highest_value)[0]+N_STEPS
+        low_indeces = np.where(all_close_points == lowest_value)[0]+N_STEPS
+    else:
+        all_high_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Top"].to_numpy()
+        all_low_points  = dataset.loc[N_STEPS:len(dataset)-FEATURE_OFFSET-1,"Bottom"].to_numpy()
+        high_indeces = np.where(all_high_points == 1)[0]+N_STEPS
+        low_indeces = np.where(all_low_points == 1)[0]+N_STEPS
 
     #如果几个最高点或最低点紧挨着，只保留连续的第一个位置
     result = []
@@ -119,69 +139,25 @@ def process_data_file(data_file_name):
     #将该图片里的所有高点采样数据加入训练集
     add_train_set(scaled_low_openclose, -1.00)
 
-    def show_sample_list(sample_list, indeces, plt_samples):
-        h_arr = np.array(sample_list)
-        h_arr = h_arr.reshape(-1, N_STEPS, 2)
-
-        for i in range(len(indeces)):
-            arr = h_arr[i]
-            arr = np.resize(arr, dataset.shape[0]*2)
-            arr = arr.reshape(-1, 2)
-            arr[N_STEPS:] = 0
-            arr = np.roll(arr, (indeces[i]-N_STEPS)*2)
-            arr = arr.reshape(-1, 2)
-            plt_samples.plot(arr)
-            plt_samples.set_xlim(0, dataset.shape[0]-1)
-            plt_samples.set_ylim(lowest_value, highest_value)
-
-    def show_high_low_samples():
-        fig, subplt_high_low_samples = plt.subplots()
-        show_sample_list(unscaled_high_openclose, high_indeces, subplt_high_low_samples)
-        show_sample_list(unscaled_low_openclose, low_indeces, subplt_high_low_samples)
-
-    
-
-    def show_high_low():
-        fig, plt_dataset_high_low = plt.subplots()
-
-        all_dataset_high_low = dataset.loc[:,["High","Low"]].to_numpy()
-        all_dataset_high_low = all_dataset_high_low.reshape(len(all_dataset_high_low), 2)
-        plt_dataset_high_low.plot(all_dataset_high_low)
-        plt_dataset_high_low.set_xlim(0, dataset.shape[0]-1)
-        plt_dataset_high_low.set_ylim(lowest_value, highest_value)
-        plt_dataset_high_low.set_title("Low-High")
-
-        for h in high_indeces:
-            plt_dataset_high_low.axvline(x=h, linestyle='dashed', color='red')
-
-        for l in low_indeces:
-            plt_dataset_high_low.axvline(x=l, linestyle='dashed', color='blue')
-
-    def show_open_close():
-        fig, plt_dataset_open_close = plt.subplots()
-        all_dataset_open_close = dataset.loc[:,["Open","Close"]].to_numpy()
-        all_dataset_open_close = all_dataset_open_close.reshape(len(all_dataset_open_close), 2)
-        plt_dataset_open_close.plot(all_dataset_open_close)
-        plt_dataset_open_close.set_xlim(0, dataset.shape[0]-1)
-        plt_dataset_open_close.set_ylim(lowest_value, highest_value)
-        plt_dataset_open_close.set_title("Open-Close")
-
-    #show_high_low_samples()    
-    #show_high_low()
-    #show_open_close()
-    #plt.show()
 
 file_pattern = "data/highlow-{}.txt"
 
 #训练集的数据文件总数
-num_files = 1600
+num_files = 2000
+
+if AUTO_MARK == True:
+    print("Using AUTO MARK, find out the top/bottom points automatically")
+else:
+    print("NOT using AUTO MARK, the top/bottom points read from data files if the columns exist")
 
 #逐个处理训练集数据文件，将训练集数据加入train_set_X_list和train_set_y_list列表
 for i in range(1, num_files + 1):
     file_name = file_pattern.format(i)
     process_data_file(data_file_name = file_name)
+    if i % 100 == 0:
+        print(f"{i}/{num_files} files processed, total {len(train_set_y_list)} samples, {n_top_samples} Top samples, {n_bottom_samples} Bottom samples")
 
-
+print(f"All training data files have been processed, total {len(train_set_y_list)} samples, {n_top_samples} Top samples, {n_bottom_samples} Bottom samples")
 
 # The LSTM architecture
 model_lstm = Sequential()
@@ -193,12 +169,14 @@ model_lstm.summary()
 
 
 model_gru = Sequential()
-model_gru.add(GRU(units=125, activation="tanh", input_shape=(N_STEPS, FEATURES)))
+model_gru.add(GRU(units=125, activation="tanh", input_shape=(N_STEPS, FEATURES), return_sequences=True))
+model_gru.add(GRU(units=125, activation="tanh"))
 model_gru.add(Dense(units=1))
 # Compiling the RNN
 model_gru.compile(optimizer="RMSprop", loss="mse")
 model_gru.summary()
 
+#训练
 X_train = np.array(train_set_X_list)
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], FEATURES)
 y_train = np.array(train_set_y_list)
@@ -206,7 +184,11 @@ y_train = y_train.reshape(-1, 1)
 #model_lstm.fit(X_train, y_train, epochs=200, batch_size=64)
 model_gru.fit(X_train, y_train, epochs=200, batch_size=32)
 
+#保存模型
+model_gru.save("gru_model.keras")
 
+
+#测试效果
 def test_X_sequence(sequence, n_steps):
     X = list()
     for i in range(len(sequence)):
@@ -229,35 +211,59 @@ def process_test_file(data_file_name):
         data_file_name
     )
 
-    #print(dataset.head())
-    #print(dataset.describe())
-
+    
     testset_total = dataset.loc[:,["Open", "Close"]].to_numpy()
 
-    plt.plot(testset_total)
-    plt.title("Prediction")
-    plt.xlabel("Time")
-    plt.ylabel("Stock Price")
-    plt.legend()
-    plt.xlim(0, len(testset_total))
-
+    
     return test_X_sequence(testset_total, N_STEPS)
 
 #用训练好的模型对测试数据集进行预测
+def process_test_file(data_file_name):
+    global dataset
+
+    dataset = pd.read_csv(
+        data_file_name, index_col="Date", parse_dates=["Date"]
+    )
+
+    
+
+    testset_total = dataset.loc[:,["Open", "Close"]].to_numpy()
+    return test_X_sequence(testset_total, N_STEPS)
+
 X_test = process_test_file("test.txt")
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], FEATURES)
 GRU_predicted_result = model_gru.predict(X_test)
-pred_high_points = np.where(GRU_predicted_result>1.0)[0]
-pred_low_points = np.where(GRU_predicted_result<-1.0)[0]
-print(f"Predicted high points: {pred_high_points}")
-print(f"Predicted low points: {pred_low_points}")
 
-for t in pred_high_points:
-    plt.axvline(x=t+N_STEPS-FEATURE_OFFSET, linestyle='dashed', color='red')
 
-for t in pred_low_points:
-    plt.axvline(x=t+N_STEPS-FEATURE_OFFSET, linestyle='dashed', color='blue')
+def make_selling_signal(signals, price):
+    signal   = []
+    i = 0
+    for value in price:
+        if i >= N_STEPS and signals[i - N_STEPS] > 1.0:
+            signal.append(value)
+        else:
+            signal.append(np.nan)
+        i = i + 1
+    return signal
 
-plt.show()
-model_gru.save("gru_model.keras")
+def make_buying_signal(signals, price):
+    signal   = []
+    i = 0
+    for value in price:
+        if i >= N_STEPS and signals[i - N_STEPS] < -1.0:
+            signal.append(value)
+        else:
+            signal.append(np.nan)
+        i = i + 1
+    return signal
+
+
+selling_signal = make_selling_signal(GRU_predicted_result, dataset['Open'])
+buying_signal = make_buying_signal(GRU_predicted_result, dataset['Open'])
+apds = [    mpf.make_addplot(selling_signal, type='scatter', markersize=200, marker='^', color='r'),
+            mpf.make_addplot(buying_signal, type='scatter', markersize=200, marker='^', color='b')
+        ]
+
+mpf.plot(dataset, type='candle',mav=(3,6,9), addplot=apds)
+
 print("All done!")
