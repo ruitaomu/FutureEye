0e0a0ed91d5f1e54ce62ba2fd45d7ea6fd3eee8b
Max Mu <ruitao.mu@gmail.com>
2023-10-01 11:41:52 +0800

    Rename predict_step.py to predict.py

--- predict.py
+++ predict.py
@@ -7,22 +7,16 @@ from sklearn.preprocessing import MinMaxScaler
 import mplfinance as mpf
 import config
 
-settings = config.load_settings()
 
-N_STEPS = settings["N_STEPS"]
-FEATURES_SET = settings["FEATURES_SET"]
-FEATURES = len(FEATURES_SET)
 
-#When OFFSET == 0, the predicted results are real happenning. Otherwise, it simulated by ideal situation
-if settings["FLOATING_POINT_ADJUSTMENT"]:
-    OFFSET = settings["FEATURE_OFFSET"] - 1
-else:
-    OFFSET = 0
 
-dataset = pd.DataFrame()
 
-def test_X_sequence(sequence, n_steps):
+def test_X_sequence(sequence, n_steps, features):
     X = list()
+    dummy = np.full((n_steps, features), np.nan).reshape(-1, features)
+    for i in range(n_steps):
+        X.append(dummy)
+
     for i in range(len(sequence)):
         end_ix = i + n_steps
         if end_ix > len(sequence) - 1:
@@ -30,64 +24,72 @@ def test_X_sequence(sequence, n_steps):
         seq_x = sequence[i:end_ix]
         
         seq_x = seq_x.flatten().reshape(-1, 1)
-        scaler = MinMaxScaler()
+        scaler = MinMaxScaler(feature_range=(0,1))
         scaler.fit(np.array([np.min(seq_x), np.max(seq_x)]).reshape(-1, 1))
         seq_x = scaler.transform(seq_x)
-        seq_x = np.array(seq_x).reshape(-1, FEATURES)
+        seq_x = np.array(seq_x).reshape(-1, features)
         X.append(seq_x)
     return np.array(X)
 
-def process_test_file(data_file_name):
+def process_test_file(settings):
     global dataset
 
     dataset = pd.read_csv(
-        data_file_name, index_col="Date", parse_dates=["Date"]
+        settings["TEST_FILE_NAME"], index_col="Date", parse_dates=["Date"]
     )
 
+    features = len(settings["FEATURES_SET"])
+
+    testset_total = dataset.loc[:,settings["FEATURES_SET"]].to_numpy()
+    return test_X_sequence(testset_total, settings["N_STEPS"], features), dataset
+
+def predict():
+    settings = config.load_settings()
     
+    features = len(settings["FEATURES_SET"])
 
-    testset_total = dataset.loc[:,FEATURES_SET].to_numpy()
-    return test_X_sequence(testset_total, N_STEPS)
+    #When OFFSET == 0, the predicted results are real happenning. Otherwise, it simulated by ideal situation
+    if settings["FLOATING_POINT_ADJUSTMENT"]:
+        OFFSET = settings["FEATURE_OFFSET"] - 1
+    else:
+        OFFSET = 0
+    
+    
+    X_test, dataset = process_test_file(settings)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], features)
+    model = keras.models.load_model(settings["MODEL_FILE_NAME"])
 
-def make_top_signals(signals, price):
-    signal   = []
-    i = 0
-    for value in price:
-        if OFFSET == 0 or i < len(signals) + N_STEPS - OFFSET:
-            if i >= N_STEPS and signals[i-N_STEPS+OFFSET] > 0.99:
-                signal.append(value)
-            else:
-                signal.append(np.nan)    
+    signals_h = []
+    signals_l = []
+    last_action = -1
+    for i in range(len(dataset)):
+        if np.all(np.isnan(X_test[i])) or i >= len(dataset) - OFFSET:
+            signals_l.append(np.nan)
+            signals_h.append(np.nan)
+            continue
+
+        sample = X_test[i+OFFSET]
+        sample = sample.reshape(1,sample.shape[0],sample.shape[1])
+        predicted_result = model.predict(sample)
+        if predicted_result[0] < 0.01 and last_action != 0:
+            signals_l.append(dataset['Open'][i])
+            last_action = 0
         else:
-            signal.append(np.nan)
-        i = i + 1
-    return signal
+            signals_l.append(np.nan)
 
-def make_bottom_signals(signals, price):
-    signal   = []
-    i = 0
-    for value in price:
-        if OFFSET == 0 or i < len(signals) + N_STEPS - OFFSET:
-            if i >= N_STEPS and signals[i-N_STEPS+OFFSET] < 0.01:
-                signal.append(value)
-            else:
-                signal.append(np.nan)
+        if predicted_result[0] > 0.99 and last_action != 1:
+            signals_h.append(dataset['Open'][i])
+            last_action = 1
         else:
-            signal.append(np.nan)
-        i = i + 1
-    return signal
+            signals_h.append(np.nan)
 
-
-X_test = process_test_file(settings["TEST_FILE_NAME"])
-X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], FEATURES)
-
-model = keras.models.load_model(settings["MODEL_FILE_NAME"])
-predicted_result = model.predict(X_test)
-signals_h = make_top_signals(predicted_result, dataset['Open'])
-signals_l = make_bottom_signals(predicted_result, dataset['Open'])
-
-apds = [    
-        mpf.make_addplot(signals_l, type='scatter', markersize=100, marker='^', color='b'),
-        mpf.make_addplot(signals_h, type='scatter', markersize=100, marker='^', color='r'),
-        ]
-mpf.plot(dataset, type='candle',mav=(3,6,9), addplot=apds)
+    apds = [    
+            mpf.make_addplot(signals_l, type='scatter', markersize=100, marker='^', color='b'),
+            mpf.make_addplot(signals_h, type='scatter', markersize=100, marker='^', color='r'),
+            ]
+    return apds
+    
+    
+if __name__ == "__main__":
+    apds = predict()
+    mpf.plot(dataset, type='candle',mav=(3,6,9), addplot=apds)
